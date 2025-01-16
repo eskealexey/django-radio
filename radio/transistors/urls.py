c5f2a971dfa8e28d017271137ceaaeb693d4c997
alexey <eskealexey@mail.ru>
2025-01-16 16:06:49 +0300

    вывод общего списка транзисторов и по типу. Пагинация на странице. Форма поиска по базе транзисторов

--- radio/transistors/urls.py
+++ radio/transistors/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import transistors_all
+from .views import transistors_all, transistors_list_tip
 
 
 urlpatterns = [
     path('', transistors_all, name='transistors_all'),
-    # path('<int:sort_id>/', sort_detail, name='sort_id'),
+    path('<int:tiptrans_id>/', transistors_list_tip, name='transistors_tip_list'),
     # path('dop/<int:info_id>/', info_detail, name='info_detail'),
     # path('found/', found, name='found'),
 ]
--- radio/transistors/views.py
+++ radio/transistors/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import TipTrans, Transistor
 
 def transistors_all(request):
     tiptrans = TipTrans.objects.all()
     transistors = Transistor.objects.all().order_by('name')
 
+    paginator = Paginator(transistors, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'tiptrans': tiptrans,
+        'title': 'Transistors List',
+        'transistors': page_obj,
+        'headword': 'Общий список транзисторов',
+    }
+    return render(request, 'transistors/transistors_list.html', context=context)
+
+
+def transistors_list_tip(request, tiptrans_id):
+    tiptrans = TipTrans.objects.all()
+    transistors = Transistor.objects.filter(tip_trans_id=tiptrans_id).order_by('name')
+
     context = {
         'tiptrans': tiptrans,
         'title': 'Transistors List',
         'transistors': transistors,
+        'headword': f'Cписок транзисторов типа "{TipTrans.objects.get(id=tiptrans_id).name}"'
     }
     return render(request, 'transistors/transistors_list.html', context=context)
-
-# Create your views here.
